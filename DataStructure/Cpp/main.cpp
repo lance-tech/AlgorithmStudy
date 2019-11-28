7a039fd457fa2bf9d00786960766b2e74e61ad4d
lance-tech <lance@lancehong.com>
2019-11-28 16:16:04 +0800

    First commit

---
+++ DataStructure/Cpp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Data Structure in C++" << std::endl;
+
+	return 0;
+}
---
+++ Sort/Cpp/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Sort in C++" << std::endl;
+
+	return 0;
+}
